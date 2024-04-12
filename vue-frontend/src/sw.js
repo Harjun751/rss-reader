348b7ac9aa628b07b0e3e701ce81ccab83241deb
unknown <Harjun751@gmail.com>
2024-04-12 14:35:58 +0800

    TLS changes

--- vue-frontend/src/sw.js
+++ vue-frontend/src/sw.js
@@ -8,13 +8,13 @@ const PRECACHE_ASSETS = [
 // Listener for the install event - pre-caches our assets list on service worker install.
 self.addEventListener('install', event => {
     event.waitUntil((async () => {
-        const cache = await caches.open(CACHE_NAME);
-        cache.addAll(PRECACHE_ASSETS);
+        // const cache = await caches.open(CACHE_NAME);
+        // cache.addAll(PRECACHE_ASSETS);
     })());
 });
 
 self.addEventListener('activate', event => {
-    event.waitUntil(self.clients.claim());
+    // event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', event => {
